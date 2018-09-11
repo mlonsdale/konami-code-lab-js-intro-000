5f55e7fbc27f717defe1d0081a5649907506d3f3
Melissa Lonsdale <melissa.lonsdale@gmail.com>
2018-09-11 13:25:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,67 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+ const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+ // [
+ //   "ArrowUp",
+ //   "ArrowUp",
+ //   "ArrowDown",
+ //   "ArrowDown",
+ //   "ArrowLeft",
+ //   "ArrowRight",
+ //   "ArrowLeft",
+ //   "ArrowRight",
+ //   "b",
+ //   "a"
+ // ];
+//
+//
+//
+// function init() {
+//   // Write your JavaScript code inside the init() function
+//   let index = 0;
+//
+//   document.body.addEventListener('keydown', function(e) {
+//       const keyPressed = e.key;
+//
+//       if (keyPressed === codes[index]) {
+//         // console.log(keyPressed);
+//         index++;
+//
+//         if (index === codes.length) {
+//
+//           window.alert("Hurray!");
+//           index = 0;
+//         }
+//       } else {
+//         index = 0;
+//
+//         }
+//
+//       })
+//
+//
+// }
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var index = -1;
 
+  document.body.addEventListener('keydown', function(e) {
+     const keyPressed = e.key;
+     console.log(keyPressed);
+
+     if (keyPressed === codes[index]) {
+         console.log(keyPressed);
+         index++;
+
+        if (index === codes.length) {
+          window.alert("Hurray!");
+          console.log("YAY!!!");
+          index = -1;
+        }
+
+     } else {
+        index = -1;
+        console.log(keyPressed);
+        console.log(" incorrect key");
+     }
+   })
 }
