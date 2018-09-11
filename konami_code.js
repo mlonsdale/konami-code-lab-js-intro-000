a3307c04ac3876a0db56fac2f59567177eb82831
Melissa Lonsdale <melissa.lonsdale@gmail.com>
2018-09-11 13:37:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -44,7 +44,7 @@
 function init() {
   var index = 0;
 
-  document.body.addEventListener('keydown', function(keyboardEvent) {
+  document.body.addEventListener('keyup', function(keyboardEvent) {
      const keyPressed = keyboardEvent.key;
      console.log(keyPressed);
 
