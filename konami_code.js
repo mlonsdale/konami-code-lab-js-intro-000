c2a56af894d3d91dcda1e7b10ac6268f4763d9a1
Melissa Lonsdale <melissa.lonsdale@gmail.com>
2018-09-11 13:29:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -49,7 +49,7 @@ function init() {
     // console.log(keyPressed);
 
      if (keys === codes[index]) {
-         console.log(event);
+         console.log(keys);
          index++;
 
         if (index === codes.length) {
