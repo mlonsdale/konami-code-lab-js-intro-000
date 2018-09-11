f361191b7a90950c4803d8dba036127aa3cec870
Melissa Lonsdale <melissa.lonsdale@gmail.com>
2018-09-11 13:45:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
  const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
  function init() {
-   let index = 0;
+   let index = -1;
 
    document.body.addEventListener('keydown', function(event) {
       const keyPressed = event.key;
@@ -14,11 +14,11 @@
          if (index === codes.length) {
            window.alert("Hurray!");
            //console.log("YAY!!!");
-           index = 0;
+           index = -1;
          }
 
       } else {
-         index = 0;
+         index = -1;
          //console.log(keys);
          //console.log(" incorrect key");
       }
