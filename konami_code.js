f90f5d57cda67c285014350c0767117011b66d4f
Melissa Lonsdale <melissa.lonsdale@gmail.com>
2018-09-11 13:55:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@
 
    document.body.addEventListener('keydown', (event) => {
       const keyPressed = event.key;
-      console.log("match " 1 + konami[length]);
+      console.log("match " + 1 + konami[length]);
 
       if (keyPressed === konami[index]) {
           console.log("pressed ")
